67698c95d48da1abb5e5a9b406ed5090940f48ff
Jason Blalock <jasonblalock@users.noreply.github.com>
2016-02-14 22:12:36 -0500

    Add attributes. Add default recipe functionality.

---
+++ attributes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: sublime_text
+# Attributes:: default
+#
+# Copyright (C) 2016 Jason Blalock
+#
+# MIT License
+#
+
+default['git']['platform']['release'] = 'trusty'
+default['git']['repository']['uri'] = 'http://ppa.launchpad.net/git-core/ppa/ubuntu'
+default['git']['repository']['keyserver'] = 'keyserver.ubuntu.com'
+default['git']['repository']['key'] = 'E1DF1F24'
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,17 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 Jason Blalock, All Rights Reserved.
+
+include_recipe 'apt'
+
+apt_repository "git-core" do
+  uri node['git']['repository']['uri']
+  components ['main']
+  distribution node['git']['platform']['release']
+  key node['git']['repository']['key']
+  keyserver node['git']['repository']['keyserver']
+  action :add
+  deb_src true
+end
+
+package 'git'
